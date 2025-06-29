4ba172d94cac3fe2789f9fdbf6763650da69ed56
Chukwudum Chukwuedo <kriz2phorus@gmail.com>
2025-06-29 22:40:00 +0100

    feat: Add Pydantic model for log entry type and raw log line item structure

--- src/log_model.py
+++ src/log_model.py
@@ -4,7 +4,8 @@ These models are used to validate and parse log entries from the input log file 
 """
 
 from enum import Enum
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
+import pendulum
 
 
 class ThresholdIndicatorTtype(str, Enum):
@@ -12,3 +13,21 @@ class ThresholdIndicatorTtype(str, Enum):
 
     WARNING = "WARNING"
     ERROR = "ERROR"
+
+
+class LogEntryType(str, Enum):
+    """Enum representing the type of log entry."""
+
+    START = "START"
+    END = "END"
+
+
+class RawLogEntry(BaseModel):
+    """Pydantic model representing a log entry from the provided log file."""
+
+    timestamp: pendulum.Time
+    job_description: str
+    log_entry_type: LogEntryType
+    pid: int
+
+    model_config = ConfigDict(arbitrary_types_allowed=True) # Adding this line to allow Pydantic to recognise pendulum.Time as a type
